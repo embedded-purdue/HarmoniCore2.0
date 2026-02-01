06f2479af4fa82b053ea0f4dfce35c7c0613ca33
Tejas Wadhwa <tejaswadhwa@gmail.com>
2026-02-01 15:12:25 -0500

    completed distortion.sv + testbench

--- sv/src/distortion.sv
+++ sv/src/distortion.sv
@@ -14,28 +14,38 @@ module distortion #(
 
 logic [17:0] mag, mag_clip, d_out, N_new, pre_addr;
 logic [35:0] mult_out;
-logic sign;
+logic sign, sign_reg;
 logic [9:0] addr; 
+logic [17:0] out_reg;
 
-multiplier_name U1(y_in, k, clk, mult_out);
+mult_gen_0 U1(y_in, K, clk, mult_out);
 
 always_comb begin
-    sign = mult_out[35] > 0;
-    mag = sign ? ~mult_out + 1 : mult_out;
-    mag_clip = mag > 4 ? 4 : mag;
+    sign = mult_out[35];
+    mag = sign ? (~mult_out + 1) : mult_out;
+    mag_clip = mag > 8192 ? 8192 : mag;  // 4.0 in Q11 format
     N_new = N - 1;
 end
 
-multiplier_name U2(mag_clip, N_new, clk, pre_addr);
+mult_gen_0 U2(mag_clip, N_new, clk, pre_addr);
 
 always_comb begin
-    addr = pre_addr >> 2; //in terms of xmax
+    addr = pre_addr >> 2; //make this in terms of xmax
 end
 
-rom_name U1(addr, clk, d_out, 1'b1);
+blk_mem_gen_0 U1(addr, clk, d_out, 1'b1);
 
-always_comb begin
-    out = sign ? ~d_out + 1 : d_out;
+// Pipeline register with negative reset
+always_ff @(posedge clk, negedge n_rst) begin
+    if (~n_rst) begin
+        sign_reg <= 1'b0;
+        out_reg <= 18'b0;
+    end else begin
+        sign_reg <= sign;
+        out_reg <= sign_reg ? ~d_out + 1 : d_out;
+    end
 end
+
+assign out = out_reg;
 
 endmodule
