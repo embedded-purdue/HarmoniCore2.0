11dad58f0e588f719fd5b12c820455059c253547
Jean-Milan Albarede <jeanmilan.albarede@gmail.com>
2026-02-15 14:54:45 -0500

    added a module header for chorus

--- sv/src/harmonic_chorus.sv
+++ sv/src/harmonic_chorus.sv
@@ -1,1 +1,16 @@
-helo wolrd
+module harmonic_chorus #(
+    parameter N = 1024,
+    parameter MAX_DELAY = 512
+)(
+    // Inputs
+    input logic clk,
+    input logic n_rst,
+    input logic [17:0] y_in, 
+
+    // Output
+    output logic [17:0] out     
+);
+
+
+
+endmodule
